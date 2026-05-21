b24e8f1a8942a532ace26c14561aa2667e714a9c
Aeqira <232300970+aeqira@users.noreply.github.com>
2026-05-21 03:46:41 +0000

    feat: update index.html title and refactor App component to implement card rendering API

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -1,66 +1,30 @@
-// src/App.tsx
+import { Hono } from "hono";
+import ACData from "adaptivecards-templating";
 
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import cloudflareLogo from "./assets/Cloudflare_Logo.svg";
-import honoLogo from "./assets/hono.svg";
-import "./App.css";
+const app = new Hono<{ Bindings: Env }>();
 
-function App() {
-	const [count, setCount] = useState(0);
-	const [name, setName] = useState("unknown");
+app.get("/api/render-card", async (c) => {
+  const cardObject = await c.env.adaptive_cards.get("member-payment-card.json");
+  const statesObject = await c.env.adaptive_cards.get("states.json");
+  const varsObject = await c.env.adaptive_cards.get("member-payment.json");
 
-	return (
-		<>
-			<div>
-				<a href="https://vite.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-				<a href="https://hono.dev/" target="_blank">
-					<img src={honoLogo} className="logo cloudflare" alt="Hono logo" />
-				</a>
-				<a href="https://workers.cloudflare.com/" target="_blank">
-					<img
-						src={cloudflareLogo}
-						className="logo cloudflare"
-						alt="Cloudflare logo"
-					/>
-				</a>
-			</div>
-			<h1>Vite + React + Hono + Cloudflare</h1>
-			<div className="card">
-				<button
-					onClick={() => setCount((count) => count + 1)}
-					aria-label="increment"
-				>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div>
-			<div className="card">
-				<button
-					onClick={() => {
-						fetch("/api/")
-							.then((res) => res.json() as Promise<{ name: string }>)
-							.then((data) => setName(data.name));
-					}}
-					aria-label="get name"
-				>
-					Name from API is: {name}
-				</button>
-				<p>
-					Edit <code>worker/index.ts</code> to change the name
-				</p>
-			</div>
-			<p className="read-the-docs">Click on the logos to learn more</p>
-		</>
-	);
-}
+  if (!cardObject || !statesObject || !varsObject) {
+    return c.json({ error: "Missing file in R2" }, 404);
+  }
 
-export default App;
+  const cardTemplateJson = await cardObject.json();
+  const states = await statesObject.json();
+  const vars = await varsObject.json();
+
+  const data = {
+    States: states,
+    Vars: vars,
+  };
+
+  const template = new ACData.Template(cardTemplateJson);
+  const renderedCard = template.expand({ $root: data });
+
+  return c.json(renderedCard);
+});
+
+export default app;
